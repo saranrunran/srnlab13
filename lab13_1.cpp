938b7c1cdd5a2137237fcfd3d274ee6ed8bd6a64
saranrunran <peemai.saranporn@gmail.com>
2026-01-06 15:48:45 +0700

    lab13_1completed

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -7,7 +7,7 @@ void stat(const double[],int,double[]);
 
 int main(){
     double A[] = {1.2,3.5,6.9,7.8,12.5,0.5};
-    int N = sizeof(A)/sizeof(A[0]);
+    int N = sizeof(A)/sizeof(A[0]); //6
     double B[6];
     stat(A,N,B);
     cout << fixed << setprecision(2);
@@ -18,4 +18,47 @@ int main(){
     cout << "Max = " << B[4] << endl;
     cout << "Min = " << B[5];
     return 0;
+}
+
+void stat(const double A[], int N, double B[]) {
+    //Mean
+    double sum = 0;
+    for (int i = 0; i < N; i++) {
+        sum += A[i];
+    }
+    B[0] = sum/N;
+    
+    //SD
+    sum = 0;
+    for (int i = 0; i < N; i++) {
+        sum += pow(A[i] - B[0],2);
+    }
+    B[1] = sqrt(sum/N);
+
+    //Geomatric Mean
+    sum = 1;
+    for (int i = 0; i < N; i++) {
+        sum *= A[i];
+    }
+    B[2] = pow(sum,1.0/N);
+
+    //Harmonic Mean
+    sum = 0;
+    for (int i = 0; i < N; i++) {
+        sum += 1/A[i];
+    }
+    B[3] = N/sum;
+
+    //Max
+    B[4] = 0;
+    for (int i = 0; i < N; i++) {
+        B[4] = max(A[i],B[4]);
+    }
+
+    //Min
+    B[5] = 2e9;
+    for (int i = 0; i < N; i++) {
+        B[5] = min(A[i], B[5]);
+    }
+    
 }
